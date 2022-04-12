f38ccedcc9fef787ad13131504882af78fecb220
Kiptoo Rugut <mccrug97@gmail.com>
2022-04-12 23:24:53 +0300

    deployment

--- awards/settings.py
+++ awards/settings.py
@@ -53,8 +53,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -109,8 +107,8 @@ TEMPLATES = [
 ]
 
 UPLOADCARE = {
-    'pub_key': '',
-    'secret': '',
+    'pub_key': config,
+    'secret': config
 }
 
 WSGI_APPLICATION = 'awards.wsgi.application'
